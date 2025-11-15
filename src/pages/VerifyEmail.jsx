1b75bbc5690d2bf086476574b35070fa9bf2975b
mkalidev <greatpannft@gmail.com>
2025-11-15 23:39:43 +0000

    style: purple hover for verify

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -79,7 +79,7 @@ export default function VerifyEmail() {
 
             <button
               type="submit"
-              className={`px-5 py-3 rounded-md text-white cursor-pointer transition-colors bg-c-color hover:bg-emerald-600`}
+              className={`px-5 py-3 rounded-md text-white cursor-pointer transition-colors bg-c-color hover:bg-purple-600`}
               onClick={(e) => {
                 e.preventDefault();
                 handleVerify();
