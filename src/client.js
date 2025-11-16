19dfcabb5c9f2fe4adc72f66db731f8039853d48
mkalidev <greatpannft@gmail.com>
2025-11-16 23:29:26 +0000

    feat: added changes

--- src/client.js
+++ src/client.js
@@ -2,5 +2,5 @@
 import { createThirdwebClient } from "thirdweb";
 
 export const client = createThirdwebClient({
-  clientId: import.meta.env.VITE_PUBLIC_THIRDWEB_CLIENT_ID,
+  clientId: import.meta.env.VITE_PUBLIC_THIRDWEB_CLIENT_ID || "f751a5613d09b119772bd8e1fb4b4cba",
 });
